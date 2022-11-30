c7cfeecd4612f47993bfed1eabb94ff7b6bcc4cc
AlekseySteblivets <alsteua@gmail.com>
2022-12-01 01:34:19 +0200

    added styles, var _coloss.scss, fonts and media queries, , installed npm sass, modern-normalize,

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+
+import 'modern-normalize/modern-normalize.css';
+
+import './assets/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
