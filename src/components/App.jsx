70c1dff73ff020b4c886e80a51b6e76eee7f6fe9
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-12-01 20:07:51 -0800

    Added registration route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,17 @@
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import DashboardPage from '../pages/DashboardPage/DashboardPage.jsx';
 import HomeTab from './HomeTab/HomeTab.jsx';
 import StatisticsDiagram from './StatisticsDiagram/StatisticsDiagram.jsx';
-
-
+import RegistrationForm from './RegistrationForm/RegistrationForm.jsx';
 
 export const App = () => {
-  
   return (
-  <Routes>
-    <Route path='/' element={<DashboardPage/>} >
+    <Routes>
+      <Route path="/registration" element={<RegistrationForm />} />
+      <Route path="/" element={<DashboardPage />}>
         <Route index element={<HomeTab />} />
         <Route path="statistics" element={<StatisticsDiagram />} />
-            
-    </Route>
+      </Route>
     </Routes>
-  
   );
 };
