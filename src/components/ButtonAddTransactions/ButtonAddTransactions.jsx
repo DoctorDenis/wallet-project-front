d66d663871ecec0aa054a6ce5e28327dba0927d8
Sergey Havrik <s.gavrik83@gmail.com>
2022-12-04 16:40:18 +0200

    add store

--- src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -1,21 +1,21 @@
 import style from './buttonAddTransactions.module.scss';
 import minus from '../../assets/images/Vectors.svg';
 import vertical from '../../assets/images/Vector 5.svg';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { changeModalStatus } from '../../redux/global/globalActions.jsx'
+import { useDispatch, useSelector } from 'react-redux';
+import { changeModalStatus } from '../../redux/global/global-actions'
 
 
 const ButtonAddTransactions = () => {
-    // const modalAddTransactionStatus = useSelector(state=>
-    //     state.global.isModalAddTransactionOpen);
-    // const dispatch = useDispatch()
+    const modalAddTransactionStatus = useSelector(state=>
+        state.global.isModalAddTransactionOpen);
+    const dispatch = useDispatch()
 
-    // function onButtonClick() {
-    //     dispatch(changeModalStatus(!modalAddTransactionStatus))
+    function onButtonClick() {
+        dispatch(changeModalStatus(!modalAddTransactionStatus))
 
-    // }
+    }
     return (        
-        <div className={style.btn}>
+        <div className={style.btn} onClick={onButtonClick} >
             <img className={style.vert} src={vertical} alt="vertical" />
             <img className={style.svg} src={minus} alt="minus" />
         </div>
