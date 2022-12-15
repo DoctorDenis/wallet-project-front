48aba66de7d565b86627c07372e3aed912b4eca5
DoctorDenis <sychovdenis@icloud.com>
2022-12-15 16:28:48 +0200

    Changed index.js  basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './assets/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/wallet-project-front">
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
