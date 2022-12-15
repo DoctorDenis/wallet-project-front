bc7cbb487c121cea2775a78ea72321a3b1342e70
zVladimirz <tzxdlo@gmail.com>
2022-12-15 17:54:33 +0200

    Сортировка
    Сортировка

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,84 +1,15 @@
 import React from 'react';
 import css from './HomeTab.module.scss';
-import { nanoid } from 'nanoid';
 import Media from 'react-media';
 import HomeTabMobile from 'components/HomeTabMobile/HomeTabMobile';
 import NotTransactions from '../../assets/images/Not.png';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTransaction } from '../../redux/transaction/transaction-operations';
-import convertStringToDate from 'utils/convertStringToDate';
-import Delete from '../../assets/images/delete-button-min.svg';
 import { deleteTransaction } from 'redux/transaction/transaction-operations';
+import SortableTable from 'components/SortableTable/SortableTable';
 
-const Table = props => {
-  const { data, deleteTrans } = props;
-  return (
-    <table className={css.table}>
-      <thead className={css.table_head}>
-        <tr className={css.tr_head}>
-          <td key={nanoid()} className={css.header_table}>
-            Date
-          </td>
-          <td key={nanoid()} className={css.header_table}>
-            Type
-          </td>
-          <td key={nanoid()} className={css.header_table}>
-            Category
-          </td>
-          <td key={nanoid()} className={css.header_table}>
-            Comment
-          </td>
-          <td key={nanoid()} className={css.header_table}>
-            Sum
-          </td>
-          <td key={nanoid()} className={css.header_table}>
-            Balance
-          </td>
-        </tr>
-      </thead>
 
-      <tbody className={css.table_body}>
-        {data?.map(row => (
-          <tr key={nanoid()} className={css.tr}>
-            <td key={nanoid()} className={css.rows}>
-              {convertStringToDate(row.date)}
-            </td>
-            <td
-              key={nanoid()}
-              className={row.isIncome ? css.rows_true : css.rows_false}
-            >
-              {row.isIncome ? '+' : '-'}
-            </td>
-            <td key={nanoid()} className={css.rows_com}>
-              {row.category}
-            </td>
-            <td key={nanoid()} className={css.rows_com}>
-              {row.comment}
-            </td>
-            <td
-              key={nanoid()}
-              className={row.isIncome ? css.rows_true : css.rows_false}
-            >
-              {row.amount.toFixed(2)}
-            </td>
-            <td key={nanoid()} className={css.rows}>
-              {row.balance.toFixed(2)}
-            </td>
-
-            <td
-              key={nanoid()}
-              onClick={() => deleteTrans(row._id)}
-              className={css.rows_del}
-            >
-              <img className={css.delete_icon} src={Delete} alt="delete" />
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
 
 const HomeTab = () => {
   const dispatch = useDispatch();
@@ -182,7 +113,7 @@ const HomeTab = () => {
               matches.table && (
                 <>
                   <div className={css.home_tab}>
-                    <Table
+                    <SortableTable
                       deleteTrans={deleteTrans}
                       data={reverseTransactions}
                     />
